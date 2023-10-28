d1ef91413281592f825277239f5fa71c64c0d450
Akshay VK <akshaykaruvathil@gmail.com>
2023-10-28 20:45:03 +0530

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h1 class="text-3xl font-bold underline">sveltekit-gh-pages</h1>
+<p>Deployed to GitHub Pages</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<a href="{base}/about">About</a>
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.700);
+  }
+</style>
